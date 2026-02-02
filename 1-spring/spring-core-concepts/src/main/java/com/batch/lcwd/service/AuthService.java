f6be964b13b00fd5b064535c4446d92aa7712616
yadav-deepakk <deepakyadav53882@gmail.com>
2026-02-02 16:16:56 +0530

    readme: code cleanup

--- 1-spring/spring-core-concepts/src/main/java/com/batch/lcwd/service/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.batch.lcwd.service;
-
-public interface AuthService {
-	public boolean loginUser() throws Exception;
-	public boolean logoutUser() throws Exception;
-}
--- 3-spring-mvc/mvc-demo/src/main/java/spring/mvc/demo/repo/CategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package spring.mvc.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import spring.mvc.demo.entities.Category;
-
-public interface CategoryRepo extends JpaRepository<Category, Integer>{
-
-}
