886259cefc1e8239e1c796bc24d016ee8071f62d
yadav-deepakk <deepakyadav53882@gmail.com>
2026-02-03 23:39:33 +0530

    lcwd: core java part 1

---
+++ 01_CoreJava/exception/Main.java
@@ -0,0 +1,41 @@
+/*
+ * ---------------------
+ *  Exception Handling
+ * ---------------------
+ *  Any unexpected or unwanted event that occurs during the program execution is
+ *  known as exception and in java there are two kind of exceptions checked and
+ *  unchecked exceptions
+ * ---------------------
+ */
+
+public class Main {
+  public static void exceptionHandling() {
+    // devide by zero exception
+    try {
+      int num1 = 0;
+      int num2 = 12;
+      int res = num2 / num1;
+      System.out.println("num2/num1 = " + res);
+    } catch (Exception e) {
+      System.out.println("exception occured: " + e.getMessage());
+      e.printStackTrace();
+    }
+
+    // array index out of bounds exception
+    try {
+      int[] arr = new int[] { 12, 21, 33 };
+      System.out.println("array 4th element: " + arr[3]);
+    } catch (Exception e) {
+      System.out.println("exception occured: " + e.getMessage());
+      e.printStackTrace();
+    }
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println("----- Exception handling -----");
+    exceptionHandling();
+    System.out.println("------------------------------");
+  }
+
+}
---
+++ 01_CoreJava/oops/Main.java
@@ -0,0 +1,49 @@
+
+/*
+ * -----------------------------------
+ * `class` and `object`
+ * `class` - class is a blueprint to create an object
+ * it consists of properties behaviours/methods.
+ *
+ * `object` - Instances of a class representing a real-
+ * world entity is known as object.
+ * -----------------------------------
+ *
+ *
+ * -----------------------------------
+ * OOPs - Object Oriented Programming
+ * -----------------------------------
+ *  1) Polymorphism
+ *  Polymorphism is a term made from `poly` and `merons`.
+ *  One thing present in many forms is known as polymorphism
+ *   - i) early binding (method overloading)
+ *   - ii) late binding (method overriding)
+ *
+ *  2) Inheritance
+ *  It is a way through which child/sub class can aquire
+ *  behaviors and properties of parent/super class.
+ *    - i) Single level - single parent class is used to
+ *    inherit the properties to derive child
+ *    - ii) Multi level - a class is derived using a derived
+ *    class
+ *    - iii) Hierarchial - multiple child classes inherting the
+ *    properties and methods of a single parent creating a
+ *    hierarchy
+ *
+ *  4) Encapsulation
+ *  Binding all members into a single unit class is known as
+ *  encapsulation
+ *
+ *  3) Abstraction
+ *  Hiding the behaviors by only giving the abstract declarations
+ *  is known as Abstraction. This is acheived using interfaces and
+ *  abstract classes in java.
+ *
+ * -----------------------------------
+ */
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("OOPs programming in java: ");
+  }
+}
