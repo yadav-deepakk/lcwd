62fc9cd2faf5e0fe0ac2efdccc9e5a0f90c8eaf1
yadav-deepakk <deepakyadav53882@gmail.com>
2026-02-14 00:01:30 +0530

    java-features: streams, optional and method reference

--- 01_CoreJava/features/Main.java
+++ 01_CoreJava/features/Main.java
@@ -1,3 +1,4 @@
+
 /*-----------------------------------------
  * lambda expressions
  * streams api
@@ -8,8 +9,14 @@
  *-----------------------------------------
  **/
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @FunctionalInterface
-interface Car {
+interface RunnableCar {
   boolean start(float fuel, boolean key);
 }
 
@@ -17,16 +24,44 @@ interface Car {
 public class Main {
 
   public static void main(String[] args) {
-
-    Car car = new Car() {
+    RunnableCar car = new RunnableCar() {
       @Override
       public boolean start(float fuel, boolean key) {
         return fuel > 0 && key;
       }
     };
 
-    // lambda expressions
-    Car car1 = (fuel, key) -> fuel > 0 && key;
+    // Lambda Expressions
+    RunnableCar car1 = (fuel, key) -> fuel > 0 && key;
+
+    // Streams API
+    // map, filter, reduce,
+    // count, min, max, sorted
+    // groupBy
+    List<Integer> list1 = new ArrayList<>();
+    list1.add(12);
+    list1.add(51);
+    list1.add(13);
+    list1.add(49);
+    list1.add(87);
+    Set<Integer> res = list1.stream().filter(x -> x % 2 == 0).map(y -> y * 2).collect(Collectors.toSet());
+    System.out.println("res: " + res);
+    int sum = list1.stream().filter(x -> x % 2 == 0).reduce(0, (a, b) -> a + b);
+    System.out.println("sum: " + sum);
+    System.out.println("count: " + list1.stream().count());
+    System.out.println("max: " + list1.stream().max(Integer::compareTo));
+    System.out.println("min: " + list1.stream().min(Integer::compareTo));
+
+    // Optional
+    Optional<Integer> optionalValue = Optional.of(null);
+    if (optionalValue.isPresent()) {
+      System.out.println("value is non null" + optionalValue.get());
+    } else {
+      System.out.println("value is null");
+    }
+
+    // Method Refereces
+    // 1)Static 2)Non-Static 3)Constructor
 
   }
 }
